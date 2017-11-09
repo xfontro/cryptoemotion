6555d8be8c65b18486dd7bdbec5ea8b9ceaa6ffb
Xavier Fontrodona <xavier.fontrodona@badiapp.com>
2017-11-09 19:34:15 +0100

    Add loop to update coins indefinately

--- cron/refresh_tokens.sh
+++ cron/refresh_tokens.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
-for fn in `cat tokens.txt`; do
-  echo -e "Refreshing ${fn}\n"
-  curl --silent http://localhost:3000/api/details/${fn}
-  sleep 30
+while :
+do
+  for fn in `cat tokens.txt`; do
+    echo -e "Refreshing ${fn}\n"
+    curl --silent http://localhost:3000/api/details/${fn}
+    sleep 60
+  done
+  sleep 1800
 done
-
