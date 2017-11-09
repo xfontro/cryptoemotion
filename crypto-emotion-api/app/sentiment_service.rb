5cf3f302f16b8c69f803d8899a4fe70c60062a7d
Xavier Fontrodona <xavier.fontrodona@badiapp.com>
2017-11-09 14:46:13 +0100

    Update emotion-api to integrate with nlp-api

--- crypto-emotion-api/app/sentiment_service.rb
+++ crypto-emotion-api/app/sentiment_service.rb
@@ -1,22 +1,26 @@
 require 'redd'
 require 'pry'
+require 'rest-client'
 
 module CryptoEmotion
   class SentimentService
     def initialize(messages:)
       @messages = messages
+      @scores = []
     end
 
     def call
+      @messages.each do |message|
+        res = RestClient.post "http://nlp-api:9000/api/score", {'message' => message}.to_json, {content_type: :json, accept: :json}
+        @scores << JSON[res.body]["compound"]
+      end
+
       calculate_string_sentiment
     end
-
-    def client
-
-    end
-
+    
+    private
     def calculate_string_sentiment
-      scores = []
+      @scores
     end
   end
 end
