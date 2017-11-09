a97199b1859282f3d9669863de4b2673ff3c57f3
Xavier Fontrodona <xavier.fontrodona@badiapp.com>
2017-11-09 23:18:42 +0100

    Fix relative dirs

--- web-client/src/app.js
+++ web-client/src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from 'datamodel/store';
-import App from './containers/App';
+import App from `${__dirname}/containers/App`;
 
 const store = configureStore();
 
