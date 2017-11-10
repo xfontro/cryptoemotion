e8118c12851d8ea00ec98c48bfd3285083d4169a
adria marzo <adriamarzo@gmail.com>
2017-11-10 09:43:26 +0100

    Allow cross origin

--- web-client/server.js
+++ web-client/server.js
@@ -6,6 +6,18 @@ const app = express();
  
 const compiler = webpack(webpackConfig);
  
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/dist'));
  
