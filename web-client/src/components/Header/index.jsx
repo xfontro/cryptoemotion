cf5ebbd66555d73b750593d5e89ae9f14f29608e
adria marzo <adriamarzo@gmail.com>
2017-11-10 10:52:25 +0100

    add bot link to website

--- web-client/src/components/Header/index.jsx
+++ web-client/src/components/Header/index.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 
 const Header = (props) =>
-  <header />
+  <header>
+    <a href="http://t.me/CryptoEmotionBot" target="_blank"></a>
+  </header>
 
 export default Header;
