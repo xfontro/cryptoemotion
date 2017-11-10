f47d223476af8c04796e32b03e20dcb1be7b640e
adria marzo <adriamarzo@gmail.com>
2017-11-10 09:53:47 +0100

    Add heroku domain to request

--- web-client/server.js
+++ web-client/server.js
@@ -2,22 +2,12 @@ const express = require('express');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
+const cors = require('cors');
 const app = express();
  
 const compiler = webpack(webpackConfig);
- 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-});
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.static(__dirname + '/dist'));
  
--- web-client/src/datamodel/MarketCoin/api/index.js
+++ web-client/src/datamodel/MarketCoin/api/index.js
@@ -3,13 +3,18 @@ import { COINMARKET_API_URL, EMOTIONS_API_URL } from 'staticdata';
 
 const header = {
   method: 'GET',
-  headers: { Accept: 'application/json', 'Content-Type': 'application/json', }
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  }
 };
 
 const getCoins = () => {
   return fetchAPIData(
     `${COINMARKET_API_URL}?limit=430`,
-    header
+    { ...header,
+      'Access-Control-Allow-Origin': 'http://cryptoemotion.herokuapp.com/',
+    }
   ).then(res => res);
 };
 
