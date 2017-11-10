33a23eae807f434f39d5ff3f50ada5b961305f04
adria marzo <adriamarzo@gmail.com>
2017-11-10 11:11:43 +0100

    FIX cors problem

--- web-client/src/datamodel/MarketCoin/api/index.js
+++ web-client/src/datamodel/MarketCoin/api/index.js
@@ -3,19 +3,18 @@ import { COINMARKET_API_URL, EMOTIONS_API_URL } from 'staticdata';
 
 const header = {
   method: 'GET',
-  headers: {
+  mode: 'cors',
+  header: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-  }
+    'Access-Control-Allow-Origin':'*',
+  },
 };
 
 const getCoins = () => {
   return fetchAPIData(
     `${COINMARKET_API_URL}?limit=430`,
-    { ...header,
-      mode: 'cors',
-      'Access-Control-Allow-Origin':'*'
-    }
+    header
   ).then(res => res);
 };
 
